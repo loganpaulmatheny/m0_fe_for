bb61e479dd39f97d9751bdd65595d5cfd84490b3
Logan Matheny <loganpaulmatheny@gmail.com>
2023-06-21 17:05:52 -0400

    Complete for statement exercises

--- for.js
+++ for.js
@@ -1,22 +1,28 @@
 // As you complete each challenge, RUN THE CODE to ensure it works. You can do this by
 // running `node <filepath>`. If you are in this directory, you'd run `node for.js`.
-  
+
 // Challenge 1
 // Use a for statement to log to the console all even numbers between 25 and 75.
 
-
-
 // Challenge 2
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
-var weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+var weather_options = [
+  "sunny",
+  "snowy",
+  "freezing",
+  "partly cloudy",
+  "drizzly",
+];
 
+for (i = 0; i < weather_options.length; i++) {
+  console.log(`The weather today is ${weather_options[i]}`);
+}
 
 // Challenge 3
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
-var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"];
 
-// 📹 Record yourself live-coding and talking through any ONE of the challenges. 
-  // The recording doesn't have to be the first time you are solving the problem. 
-  // Use technical vocabulary and explain each part to demonstrate your understanding! 
-  // Share the link to your video in your small group channel.
-
+// 📹 Record yourself live-coding and talking through any ONE of the challenges.
+// The recording doesn't have to be the first time you are solving the problem.
+// Use technical vocabulary and explain each part to demonstrate your understanding!
+// Share the link to your video in your small group channel.
--- selfEvaluation.md
+++ selfEvaluation.md
@@ -1,6 +1,6 @@
 # Self Evaluation
 
-- How confident do you feel in your understanding and fluency with the `for` statement?
-- This was a much more advanced concept than anything you've done in Mod 0, and the exercises required you to push yourself to apply other background knowledge or learn things outside of the curriculum - how did that feel? Did you have productive struggle? Unproductive struggle?
-- What do you still need to practice or learn? How will you do that? Do you need to adjust your calendar in order to do that?
-- What questions do you still have? How will you get the answers you need?
+- How confident do you feel in your understanding and fluency with the `for` statement? **Feeling good!**
+- This was a much more advanced concept than anything you've done in Mod 0, and the exercises required you to push yourself to apply other background knowledge or learn things outside of the curriculum - how did that feel? Did you have productive struggle? Unproductive struggle? **Mostly productive struggle. I enjoyed re-learning and practicing with for loops since I haven't been using them for a while. I also found the `startsWith()` method!**
+- What do you still need to practice or learn? How will you do that? Do you need to adjust your calendar in order to do that? **Nothing at the moment! **
+- What questions do you still have? How will you get the answers you need? **None at this time **
